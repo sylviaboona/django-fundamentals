597a04ca39d5f51db2bf503fe742a8deef32a607
sylviaboona <sylviaboona2@gmail.com>
2022-01-24 22:03:05 +0300

    sorting data

--- playground/views.py
+++ playground/views.py
@@ -1,14 +1,17 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.db.models import Q, F
 from store.models import Product
 from django.core.exceptions import ObjectDoesNotExist
 
 #MANAGING OBJECTS
-def say_hello(request):
+def say_hello1(request):
     query_set = Product.objects.all()
     list(query_set)
     query_set[0:5]
-    query_set.filter().filter().order_by()
+    # query_set.filter().filter().order_by()
+    return render(request, 'hello.html', {'products': query_set})
+    
 
 #RETRIEVING OBJECTS
 def hello(request):
@@ -26,6 +29,44 @@ def hello(request):
 
 #APPLYING MULTIPLE FILTERS
 def hello1(request):
+    pass
+
+
+#FILTERING DATA
+def say_hello2(request):
+    query_set = Product.objects.all()
+    products = query_set.filter(unit_price__range=(20, 30)) #filter number
+    products2 = query_set.filter(title__contains='Coffee') #filter strings
+    products3= query_set.filter(title__icontains='coffee') #to make the search non case sensitive use 'icontains'
+    products3= query_set.filter(title__istartswith='coffee')
+    # products3= query_set.filter(title__iendswith='coffee')
+    products3= query_set.filter(last_update__year=2022)
+    products3= query_set.filter(description__isnull=True)
+    
+    return render(request, 'hello.html', {'products': products3}) #filter strings
+
+
+#COMPLEX FILTERING
+def say_hello3(request):
     #Products: inventory < 10 and price < 20
     product = Product.objects.filter(inventory__lt=10, unit_price__lt=20)
     product1 = Product.objects.filter(inventory__lt=10).filter(unit_price__lt=20)
+    products2 = Product.objects.filter(Q(inventory__lt=10) | Q(unit_price__lt=20))
+    # query_set.filter().filter().order_by()
+    
+    return render(request, 'hello.html', {'products': products2})
+
+
+#REFERENCING FIELDS USING F OBJECTS
+#used to compare two fields in a model
+def  say_hello4(request):
+    # e.g. Product: inventory=price
+    query_set = Product.objects.filter(inventory=F('unit_price'))
+    return render(request, 'hello.html', {'products': query_set})
+
+
+
+#SORTING DATA
+def  say_hello(request):
+    query_set = Product.objects.order_by('title')
+    return render(request, 'hello.html', {'products': query_set})
